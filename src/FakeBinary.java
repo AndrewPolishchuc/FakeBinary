d3aa656473ba0161c0d64116a91dfbce0e33c34e
38093 <olkftrylk2@gmail.com>
2020-07-22 10:39:10 +0300

    Done

--- src/FakeBinary.java
+++ src/FakeBinary.java
@@ -1,5 +1,25 @@
 public class FakeBinary {
     public static void main(String[] args) {
-
+        System.out.println(fakeBin("5ajbkgag1623asbkasjg61361"));
+    }
+    public static String fakeBin(String numberString){
+        if (numberString== null || numberString.length() == 0)
+            return "";
+        String returnString = new String();
+        for (int index = 0; index < numberString.length(); index++) {
+            char c = numberString.charAt(index);
+            if (!(c >= '0' && c <= '9')) {
+                returnString += c;
+                continue;
+            }
+            if (c < '5'){
+                returnString+= "0";
+                continue;
+            }
+            if (c >= '5'){
+                returnString+= "1";
+            }
+        }
+        return returnString;
     }
 }
